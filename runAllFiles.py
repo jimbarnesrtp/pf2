9e9b7bb639638395da1c9c7ac5af50a24b779c67
JIm Barnes <direrat@gmail.com>
2020-02-10 16:56:47 -0500

    Added script to call all in procession to rebuild all files

---
+++ runAllFiles.py
@@ -0,0 +1,35 @@
+import datetime
+
+pf2Files = ["buildAdvGear2.py","buildAlchemicalItems.py", "buildSpellsv2.py", "buildWornItems.py", "buildWeapons.py", "buildWands.py", "buildTraits.py", "buildStructures.py", 
+    "buildStaves.py", "buildSnares.py", "buildSkills.py", "buildShields.py", "buildServices.py", "buildRunes.py", "buildRituals.py", "buildMonsters.py", "buildMaterials.py",
+    "buildHeldItems.py", "buildHazards.py", "buildDomains.py", "buildDeities.py", "buildConsumables.py", "buildConditions.py", "buildCompanions.py", "buildBackgrounds.py", 
+    "buildArmor.py", "buildAncestryFeats.py", "buildFeats.py"]
+
+
+dateString = datetime.date.today().strftime("%Y%m%d")
+statusFile = "pf2Update-"+dateString+".txt"
+
+from pathlib import Path
+
+processedFiles = []
+
+my_file = Path(statusFile)
+print(statusFile)
+if my_file.is_file():
+    with open(statusFile, "r") as r:
+        processedFiles = [line.rstrip() for line in r]
+        print(processedFiles)
+
+for fileName in pf2Files:
+    if fileName not in processedFiles:
+        exec(open(fileName).read())
+        processedFiles.append(fileName)
+    
+
+
+
+f = open(statusFile, "w")
+for name in processedFiles:
+    f.write(name+"\n")
+    
+f.close
