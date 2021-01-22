5e9514a4dc5914ef61471715843b38645b638362
meta-aeon <33475968+meta-aeon@users.noreply.github.com>
2021-01-21 22:24:00 -0700

    Fit current source, list/dict-ify some items
    Possible TODOs: 
    - Grab and handle separately faiths/philosophies like Green Faith.
    - Should "Divine Ability" be listified?  If so, some gods (like Asmodeus) might need special handling.

--- buildDeities.py
+++ buildDeities.py
@@ -9,6 +9,20 @@ deityHolder = {}
 deityHolder['name'] = 'Pathfinder 2.0 deities list'
 deityHolder['date'] = datetime.date.today().strftime("%B %d, %Y")
 
+def encoder(words):
+    encoded = words.replace(u"\u2019", "'")
+    encoded1 = encoded.replace(u"\u2014", "-")
+    encoded2 = encoded1.replace(u"\u2011", " ")
+    encoded3 = encoded2.replace(u'\u201c', "'")
+    encoded4 = encoded3.replace(u'\u201d', "'")
+    encoded5 = encoded4.replace(u'\u2026', "...")
+    encoded6 = encoded5.replace(u'\u2013', "-")
+    encoded7 = encoded6.replace(u'\u2018', "'")
+    encoded8 = encoded7.replace(u'\ufb01', "fi")
+    encoded9 = encoded8.replace(u'\ufb02', "fl")
+    encoded10 = encoded9.replace(u'\u00d7', "x")
+    return encoded10
+
 def get_details(link):
     itemDetails = {}
     res = requests.get(link)
@@ -16,58 +30,106 @@ def get_details(link):
     soup = BeautifulSoup(res.text, 'lxml')
     feat = soup.find_all("div", {'class':'main'})
     detail = soup.find("span", {'id':'ctl00_MainContent_DetailedOutput'})
-    #print(detail.contents)
     children = detail.contents
+    skipTitle = False
+    if children[0].find("a").get('href') == "PFS.aspx":#Only some entries have PFS tags, and if they do then the HTML of the source is broken.  So we break out of it, and then do handling for any issues this causes later.
+        children = detail.next_sibling.parent.contents
+        skipTitle = True
     reachedBreak = False
     reachedCrit = False
+    reachedIntercession = False
     detailHolder = []
     tagType = ""
     string = " "
-    fontHolder = []
     spellHolder = []
+    divinterDesc = ""
+    domainHolder = []
+    altDomains = []
+    alreadyHandled = ["Follower Alignments", "Favored Weapon", "Divine Font"]
     for child in children:
+        #print (str(child))
         stringContents = str(child)
         if stringContents.startswith("<"):
 
             if child.name == "a":
                 try:
-                    if child['class'][0] == "external-link" : 
-                        itemDetails['source'] = child.text
+                    if child['class'][0] == "external-link":
+                        if reachedIntercession == False:
+                            itemDetails['source'] = encoder(str(child.text))
+                        else:
+                            itemDetails['divinterSource'] = encoder(str(child.text))
+                        tagType = ""
                 except:
                     pass
-                tagType = ""
             if child.name == "b":
-                tagType = child.text
-            if child.name == "i":
-                if tagType == "Divine Font":
-                    fontHolder.append(child.text)
+                tagType = encoder(str(child.text))
+            if ((child.name == "i") or ((child.name == "a") and ((child.get("style") == "text-decoration:underline") or (child.find('u'))))) and (tagType not in alreadyHandled):
                 if tagType == "Cleric Spells":
-                    spellHolder.append(child.text)
+                    spellHolder.append(encoder(child.text))
+                elif tagType == "Domains":
+                    domainHolder.append(encoder(child.string))
+                elif tagType == "Alternate Domains":
+                    altDomains.append(encoder(child.string))
+                else:
+                    if itemDetails.get(tagType):#If this tag already has something, we fuse the text with the existing something instead of setting it
+                        if child.find("i"):
+                            itemDetails[tagType] = (str(itemDetails[tagType]) + "*" + encoder(str(child.string)) + "*")#Markdown seems like a good way to handle *italcs*.
+                        else:#In this case it's probably a link tag, which we just want the plain text of.
+                            itemDetails[tagType] = (str(itemDetails[tagType]) + encoder(str(child.string)))
+                    elif tagType == "":#If we haven't yet hit a tag, then we assume we're in the main text block, and append it to detailHolder.
+                        if child.name == "i":
+                            detailHolder.append("*" + encoder(str(child.string)) + "*")
+                        else:
+                            detailHolder.append(encoder(str(child.string)))
+                    else:
+                        itemDetails[tagType] = encoder(str(child.string))
+            if (child.name == "h2") and (child.text == "Divine Intercession"):
+                reachedIntercession = True
         else:
 
             if tagType != "":
-                if (tagType != "Follower Alignments" and tagType != "Favored Weapon" and tagType != "Domains"):
+                if (tagType not in alreadyHandled):
                     if not stringContents.isspace():
-                        if tagType == "Cleric Spells":
-                            spellHolder.append(stringContents)
-                        elif tagType == "Divine Font":
-                            fontHolder.append(stringContents)
+                        if (tagType == "Cleric Spells"):
+                            if (child.next_sibling.name != None) and (child.next_sibling.name == 'i'): #Only grabs spells until the next thing isn't in italics (we expect spells to be italicized) 
+                                spellHolder.append(encoder(stringContents))
+                            elif reachedIntercession == True:
+                                divinterDesc = divinterDesc + encoder(str(child.string))
+                            elif reachedIntercession == False:#If we're still in the main block, there must be some condition on the spell gained, like Apsu's "(metallic dragons only)"
+                                spellHolder[-1] = str(spellHolder[-1] + encoder(str(child.string)))#...So we fuse it to the most recent spell entry.
+                            else:
+                                print ("Unhandled text: " + str(child))
+                        elif tagType in ("Domains", "Alternate Domains"):#We don't want this text, as it's just commas.
+                            pass
                         else:
-                            itemDetails[tagType] = stringContents
+                            if itemDetails.get(tagType):
+                                itemDetails[tagType] = (str(itemDetails[tagType]) + encoder(str(child.string)))
+                            else:
+                                itemDetails[tagType] = encoder(str(child.string))
             else:
                 if not stringContents.isspace():
-                    detailHolder.append(stringContents)
+                    if (not detailHolder) and (skipTitle == True):#If there's nothing already in detailHolder, then we might need to skip the deity's title.
+                        detailHolder.append(" ")
+                    elif reachedIntercession == False:
+                        detailHolder.append(encoder(stringContents))
 
        #print(child)
        #print('<!!!!!!!!!!!!!!!!!!!!!!!!!>')
-    itemDetails['fontHolder'] = string.join(fontHolder)
-    itemDetails['clericSpells'] =string.join(spellHolder)
-    itemDetails['text'] = string.join(detailHolder)
+    spellHolder = [spellEntry.strip(",:").strip() for spellEntry in spellHolder]#strips ,: and whitespace from the spellHolder
+    spellIter = iter(spellHolder)
+    spellDict = dict(zip(spellIter, spellIter))#Dict-ifies the spells, output formatted like: {'1st': 'sleep', '4th': 'fly"}
+    for itemEntry in itemDetails:
+        itemDetails[itemEntry] = itemDetails[itemEntry].strip(",:").strip()
+    itemDetails['domains'] = domainHolder
+    itemDetails['altDomains'] = altDomains
+    itemDetails['clericSpells'] = spellDict
+    itemDetails['text'] = "".join(detailHolder).strip(', ').strip()
+    itemDetails['divinterDesc'] = divinterDesc.strip()
     return itemDetails
 
 
 
-def get_domains(link):
+def get_entry(link):
     items = []
     multiItems = []
     res2 = requests.get(link)
@@ -84,12 +146,23 @@ def get_domains(link):
         entries = row.find_all(lambda tag: tag.name=='td')
         if entries is not None:
             if len(entries) > 0:
-                name = entries[0].find("a").text
+                if (entries[0].find("a").get('href') == "PFS.aspx"):#Not all entries have PFS tags, so we have to handle them seperately
+                    name = entries[0].find("a").find_next("a").text
+                    link = "https://2e.aonprd.com/" + entries[0].find("a").find_next("a").get('href')
+                    pfsLegal = entries[0].find("a").img.get('alt')
+                else:
+                    name = entries[0].find("a").text
+                    link = "https://2e.aonprd.com/" + entries[0].find("a").get('href')
+                    pfsLegal = 'No PFS Data'
                 item['name'] = name
-                item['link'] = "https://2e.aonprd.com/"+entries[0].find("a")['href']
-                item['alignment'] = entries[1].text.split(",")
-                item['followerAlignments'] = entries[2].text.replace(u'\u2014', '').split(",")
-                item['domains'] = entries[3].text
+                item['link'] = link
+                item['PFS'] = pfsLegal
+                item['category'] = entries[1].text.strip()
+                item['alignment'] = entries[2].text.split(",")
+                item['followerAlignments'] = entries[3].text.replace(u'\u2014', '').split(", ")
+                item['font'] = entries[4].text.split("/")
+                #item['domains'] = entries[5].text.split(", ")#Commented out because we handle this in get_details.
+                item['weapon'] = entries[6].text.split(" or ")
 
                 print("getting details for:",item['name'])
                 itemDetails = get_details(item['link'])
@@ -106,7 +179,7 @@ def get_domains(link):
 
 
 def get_all():
-    deityHolder['deities'] = get_domains("https://2e.aonprd.com/Deities.aspx")
+    deityHolder['deities'] = get_entry("https://2e.aonprd.com/Deities.aspx")
 
     #deityHolder['rangedWeapons'] = get
 
